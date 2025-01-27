0d54bd49f3afcf32637dcc77d96ae144da2c894c
tayier <ntparis9@gmail.com>
2025-01-27 11:19:02 +0100

    add : header css

--- src/presentation_Layer/components/header/Header.jsx
+++ src/presentation_Layer/components/header/Header.jsx
@@ -1,7 +1,21 @@
+import styles from "./Header.module.scss";
+
+
 export default function Header() {
 	return (
-	  <div id="header">
-		<h3>header</h3>
-	  </div>
-	);
+    <header id={styles["header"]}>
+      <img
+        src="src/presentation_Layer/assets/logo-SportSee.svg"
+        alt="logo"
+      ></img>
+      <nav className="nav-top">
+        <ul className="nav-top-list">
+          <li>Accueil</li>
+          <li>Profil</li>
+          <li>Réglage</li>
+          <li>Communauté</li>
+        </ul>
+      </nav>
+    </header>
+  );
   }
