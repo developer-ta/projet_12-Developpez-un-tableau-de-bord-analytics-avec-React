119b034312de47ba2a5384a87259ee606e69e000
tayier <ntparis9@gmail.com>
2025-01-27 01:11:19 +0100

    add : sideBarcomponent css

--- src/presentation_Layer/components/side/SideBar.jsx
+++ src/presentation_Layer/components/side/SideBar.jsx
@@ -1,9 +1,21 @@
 import styles from "./SideBar.module.scss";
-
+const listIconNavL = [
+  { srcIcon: "src/presentation_Layer/assets/icon-1.svg" },
+  { srcIcon: "src/presentation_Layer/assets/icon-2.svg" },
+  { srcIcon: "src/presentation_Layer/assets/icon-3.svg" },
+  { srcIcon: "src/presentation_Layer/assets/icon-4.svg" },
+];
 export default function SideBar() {
   return (
-    <div id="sideBar">
-      <h3>sideBar</h3>
-    </div>
+    <nav id={styles["sideBar"]}>
+      <ul className="NavLeft">
+        {listIconNavL.map(({ srcIcon }) => (
+          <li className="icon-Nav-l">
+            <img src={srcIcon} alt="icon-Nav-l" />
+          </li>
+        ))}
+      </ul>
+      <p>Copiryght, SportSee 2020</p>
+    </nav>
   );
 }
